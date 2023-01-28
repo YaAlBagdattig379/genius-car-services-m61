7f552a9b1e746967ea271225cbabf2146723553b
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-01-28 16:57:31 +0530

    v6 to get value from input

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -9,15 +9,18 @@ const Register = () => {
     }
     const handleRegister = event =>{
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(name,email,password)
     }
     return (
         <div className='form-register'>
             <h3 className='text-center'>Please Register</h3>
             <form onSubmit={handleRegister}>
                 <input type="text" name="name"required placeholder='Your Name' id="" />
-                <input type="email"required placeholder='Email Address' name="" id="" />
-                <input type="password" name=""required placeholder='Password' id="" />
-                <input type="Password"required placeholder='Confirm-Password' name="" id="" />
+                <input type="email"name="email" required placeholder='Email Address' id="" />
+                <input type="Password"name="password" required placeholder='Confirm-Password' id="" />
                 <input type="submit"value={'Register'}></input>
             </form>
             <p>Aready have an account? <Link to={'/login'} onClick={navigateToLogin}  className='text-danger pe-auto text-decoration-none '>Please Login</Link></p>
